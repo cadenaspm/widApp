7270a83e56fb16ff4939c28e39efdf55eb6cd2c5
motrov91 <manuelcadenaspm@gmail.com>
2025-08-08 15:02:40 -0500

    implemented homescreen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,14 +1,56 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-      title: Text('Flutter + Material 3'),
-     ),
+      appBar: AppBar(title: Text('Flutter + Material 3')),
+      body: _HomeView(listItems: appMenuItems),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  final List listItems;
+
+  final listItemStructure = const ListTile(
+    leading: Icon(Icons.smart_button_outlined),
+    title: Text('Title'),
+    subtitle: Text('Subtitle'),
+    trailing: Icon(Icons.arrow_forward_ios_outlined),
+  );
+
+  const _HomeView({required this.listItems});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: listItems.length,
+      itemBuilder: (context, index) => _ItemListTile(menuItem: listItems[index],),
+    );
+  }
+}
+
+class _ItemListTile extends StatelessWidget {
+  const _ItemListTile({
+    required this.menuItem,
+  });
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      trailing: Icon(Icons.arrow_forward_ios_outlined, color: colors.primary),
+      onTap: () {},
     );
   }
 }
