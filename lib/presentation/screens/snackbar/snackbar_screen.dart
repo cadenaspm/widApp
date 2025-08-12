a3a76b32053d36236264cb70cc071a9d0ba87bb4
motrov91 <manuelcadenaspm@gmail.com>
2025-08-12 14:57:34 -0500

    working with dialogs

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -20,6 +20,10 @@ class SnackbarScreen extends StatelessWidget {
     ScaffoldMessenger.of(ctx).showSnackBar(snackbar);
   }
 
+  void openDialog(BuildContext ctx){
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +34,29 @@ class SnackbarScreen extends StatelessWidget {
         onPressed: () => showCustomSnackbar(context),
         label: Text('Mostrar snackbar'),
         icon: const Icon(Icons.remove_red_eye_outlined),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: (){
+                showAboutDialog( 
+                  context: context,
+                  children: [
+                   Text(' Labore incididunt ipsum dolore pariatur sint. Amet sint consequat laborum sint irure dolor elit quis sunt. Anim reprehenderit velit nostrud sint cillum eiusmod. Incididunt anim eu ut enim mollit ex adipisicing do nostrud anim ipsum. Culpa elit eiusmod ea enim ipsum reprehenderit ex culpa cupidatat fugiat dolor nostrud non Lorem. Tempor qui eiusmod proident non.')
+                  ]
+                );
+              }, 
+              child: const Text('Licencias Usadas')
+            ),
+
+            FilledButton.tonal(
+              onPressed: (){}, 
+              child: Text('Mostrar dialogo')
+            )
+          ],
+        ),
       ),
     );
   }
